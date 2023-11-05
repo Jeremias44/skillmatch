18dce914dcc4a1121e33514920ae8aace17e17dd
Jeremias44 <jeremiaspombo@outlook.com>
2023-11-05 04:20:19 -0300

    feat:

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -22,40 +22,29 @@ from PIL import Image
 # Obtener la ruta absoluta del directorio actual
 current_directory = os.path.dirname(__file__)
 # Construir la ruta completa a la imagen 1
-image_path = os.path.join(current_directory, 'src', '1.png')
+image_path = os.path.join(current_directory, 'media', '1.png')
 # Cargar la imagen y mostrarla
 imagen = Image.open(image_path)
 st.image(imagen, caption='', use_column_width=True)
 # Construir la ruta completa a la imagen 2
-image_path = os.path.join(current_directory, 'src', '2.png')
+image_path = os.path.join(current_directory, 'media', '2.png')
 # Cargar la imagen y mostrarla
 imagen = Image.open(image_path)
 st.image(imagen, caption='', use_column_width=True)
 
-
 # Crear una columna lateral
 # Construir la ruta completa a la imagen 3
-image_path = os.path.join(current_directory, 'src', '3.png')
+image_path = os.path.join(current_directory, 'media', '3.png')
 # Cargar la imagen y mostrarla en la columna lateral
 imagen2 = Image.open(image_path)
 st.sidebar.image(imagen2, caption='', use_column_width=True)
 
-
 usuario = st.sidebar.text_input("Usuario")
 clave = st.sidebar.text_input("Clave", type="password")
-
-
-st.sidebar.markdown("### Contacta con el autor del sitio")
-st.sidebar.markdown("[Jeremías Pombo en LinkedIn](https://www.linkedin.com/in/jeremiaspombo/)")
-st.sidebar.markdown("### Visita el repositorio del proyecto")
-st.sidebar.markdown("[Repositorio de GitHub](https://github.com/Jeremias44/skillmatch)")
 
 # Verificar las credenciales
 if (usuario == 'usuario1' and clave == "clave1"):
     st.sidebar.write("¡Acceso permitido!")
-
-
-
 
     #Ejemplos de llenado (Help)
     experiencias_help = "Texto de Ejemplo: Que tenga experiencia en análisis de datos, machine learning y big data"
@@ -67,10 +56,8 @@ if (usuario == 'usuario1' and clave == "clave1"):
     # Caja para elegir un número del 1 al 50
     k = st.number_input("Escribí el número de CVs que deseás traer (1 a 50)", min_value=1, max_value=50, value=3)
 
-
     # Filtro
     opcion = st.multiselect("Indicá a la base de datos de cuál o cuáles usuarios deseás acceder (1 a 5) 🔍:", ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"])
-
 
     query = f' {experiencias} {herramientas}'
     docs=docSearch.similarity_search(query, k=k, filter=
@@ -95,11 +82,20 @@ if st.button("Ver Currículums Seleccionados"):
     st.write('🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀')
     st.subheader('🚀🚀 Estos son los Match de tu Búsqueda Actual 🚀🚀')
     st.write('🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀')
-    for i, doc in enumerate(docs):
-        st.write(f'Documento {i+1}: ')
-        st.write(doc.metadata['name'])
-        st.write(doc.page_content)
-        st.write('🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀')
+    try:
+        for i, doc in enumerate(docs):
+            st.write(f'Documento {i+1}: ')
+            st.write(doc.metadata['name'])
+            st.write(doc.page_content)
+            st.write('🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀')
+    except:
+        st.write('Se solicita acceder con credenciales (usuario y clave)')
 
 else:
     st.sidebar.write("Sin acceso. Coloque un usuario y clave válidos o ingrese como invitado")
+
+
+st.sidebar.markdown("### Contacta con el autor del sitio")
+st.sidebar.markdown("[Jeremías Pombo en LinkedIn](https://www.linkedin.com/in/jeremiaspombo/)")
+st.sidebar.markdown("### Visita el repositorio del proyecto")
+st.sidebar.markdown("[Repositorio de GitHub](https://github.com/Jeremias44/skillmatch)")
